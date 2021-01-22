a44891dc60d2a5ce3c4b3adb3924ab081bc869ce
jorcontrerasp <77851630+jorcontrerasp@users.noreply.github.com>
2021-01-22 19:24:56 +0100

    Proyecto ScriptLapseExe
    Se añade el proyecto ScriptLapseExe al repositorio.

---
+++ ScriptLapseExe/src/scriptlapseexe/ScriptLapseExe.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scriptlapseexe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Timestamp;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jorge
+ */
+public class ScriptLapseExe {
+
+    private static final boolean EJECUTAR = true;
+    private final static Logger LOG = Logger.getLogger(ScriptLapseExe.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LOG.log(Level.INFO, "Arrancando hilo de ejecución");
+        long minuto = 60 * 1000;
+        long nMinutos = 90;
+        if(args!=null && args.length>0){
+            nMinutos = (long)Integer.parseInt(args[0]);
+        }
+        long tEspera = 0;
+
+        long time = 0;
+        int numEjecucion = 1;
+
+        while (EJECUTAR) {
+            while (!((System.currentTimeMillis() - time) < tEspera)) {
+                try {
+                    time = System.currentTimeMillis();
+                    LOG.log(Level.INFO, "Iniciando ejecución número " + numEjecucion);
+                    
+                    ejecuta();
+                    
+                    if (tEspera == 0) {
+                        tEspera = minuto * nMinutos;
+                    }
+                    LOG.log(Level.INFO, "Ejecución número " + numEjecucion + " finalizada");
+
+                    numEjecucion++;
+                    Timestamp ts = new Timestamp(System.currentTimeMillis() + tEspera);
+                    String t = ts.toString().substring(0, 19);
+                    LOG.log(Level.INFO, "Inicio de la ejecución número " + numEjecucion + ": " + t);
+
+                } catch (Exception e) {
+                    LOG.log(Level.SEVERE, "Se ha producido un error. Revise la acción.", e);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void ejecuta() {
+        Process process;
+        try {
+            Runtime runtime = Runtime.getRuntime();
+
+            process = runtime.exec("./tarea.sh");
+
+            while (process.isAlive()) {
+                Timestamp ts = new Timestamp(System.currentTimeMillis());
+                String t = ts.toString().substring(0, 19);
+                System.out.println(t + " - ejecutando proceso...");
+                Thread.sleep(10000);
+            }
+
+            System.out.println("");
+
+            imprimirOutputProcess(process);
+
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, null, e);
+        }
+    }
+
+    public static void imprimirOutputProcess(Process p) {
+        try {
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            int exitVal = p.waitFor();
+            System.out.println("Process exitValue: " + exitVal);
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, null, e);
+        }
+    }
+}
